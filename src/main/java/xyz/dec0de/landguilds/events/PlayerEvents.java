318fb6e086e166fc8d1f3480e39e383a229ebc3a
bradenn <braden@bradencnicholson.com>
2020-04-08 10:50:48 -0700

    You can chat in guilds with using a tilde  as the first index of your message

--- src/main/java/xyz/dec0de/landguilds/events/PlayerEvents.java
+++ src/main/java/xyz/dec0de/landguilds/events/PlayerEvents.java
@@ -20,12 +20,24 @@ public class PlayerEvents implements Listener {
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent e) {
+
         try {
             PlayerStorage playerStorage = new PlayerStorage(e.getPlayer());
-            if (playerStorage.getGuild() != null) {
-                e.setFormat(e.getFormat().replace("{GUILD}", playerStorage.getGuild().getTag()));
+            if (e.getMessage().charAt(0) == '`') {
+                e.setCancelled(true);
+                playerStorage.getGuild().getMembers().forEach(member -> {
+                    Player target = e.getPlayer().getServer().getPlayer(member);
+                    if(target != null){
+                        target.sendMessage(e.getPlayer().getDisplayName() + " §c->§r " + playerStorage.getGuild().getColor() + playerStorage.getGuild().getName() + "§8:§r " + e.getMessage().substring(1));
+                    }
+                });
             } else {
-                e.setFormat(e.getFormat().replace("{GUILD}", ""));
+                if (playerStorage.getGuild() != null) {
+
+                    e.setFormat(e.getFormat().replace("{GUILD}", playerStorage.getGuild().getTag()));
+                } else {
+                    e.setFormat(e.getFormat().replace("{GUILD}", ""));
+                }
             }
         } catch (IOException ex) {
             ex.printStackTrace();
